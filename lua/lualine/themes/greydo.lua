9c83863ec7bc3074c7936d0f391dae036b865882
John Norén <super.happy.email.fun@gmail.com>
2024-06-14 20:09:55 +0200

    test

--- lua/lualine/themes/greydo.lua
+++ lua/lualine/themes/greydo.lua
@@ -4,53 +4,45 @@ local greydo = require('lush_theme.greydo')
 local base_colors = require('lush_theme.colors')
 
 local colors = {
-    -- black        = tostring(greydo.Normal.bg),
-    -- white        = tostring(greydo.Normal.fg),
-    -- red          = tostring(greydo.Constant.fg),
-    -- green        = tostring(base_colors.green),
-    -- blue         = tostring(greydo.Normal.fg.rotate(186).saturate(100)),
-    medium_blue  = tostring(greydo.Comment.fg),
-    -- yellow       = tostring(base_colors.yellow),
-    black        = '#a89984',
-    white        = '#a89984',
-    red          = '#a89984',
-    green        = '#a89984',
-    blue         = '#a89984',
-    yellow       = '#a89984',
+    green        = tostring(base_colors.green),
+    red        = tostring(base_colors.red),
+    yellow       = tostring(base_colors.yellow),
+    medium_blue  = tostring(greydo.LineNr.fg),
+    dark_blue  = tostring(greydo.LineNr.fg.darken(10)),
+    bg = tostring(greydo.Normal.bg),
+    fg = tostring(greydo.Normal.fg),
     gray         = '#a89984',
     darkgray     = '#3c3836',
-    lightgray    = '#504945',
-    inactivegray = '#7c6f64',
 }
 return {
     normal = {
-        a = { bg = colors.medium_blue, fg = colors.black },
-        b = { bg = colors.lightgray, fg = colors.white },
-        c = { bg = colors.darkgray, fg = colors.gray }
+        a = { bg = colors.medium_blue, fg = colors.bg },
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     },
     insert = {
-        a = { bg = colors.green, fg = colors.black },
-        b = { bg = colors.lightgray, fg = colors.white },
-        c = { bg = colors.lightgray, fg = colors.white }
+        a = { bg = colors.green, fg = colors.bg },
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     },
     visual = {
         a = { bg = colors.yellow, fg = colors.black },
-        b = { bg = colors.lightgray, fg = colors.white },
-        c = { bg = colors.inactivegray, fg = colors.black }
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     },
     replace = {
         a = { bg = colors.red, fg = colors.black },
-        b = { bg = colors.lightgray, fg = colors.white },
-        c = { bg = colors.black, fg = colors.white }
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     },
     command = {
         a = { bg = colors.green, fg = colors.black },
-        b = { bg = colors.lightgray, fg = colors.white },
-        c = { bg = colors.inactivegray, fg = colors.black }
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     },
     inactive = {
         a = { bg = colors.darkgray, fg = colors.gray },
-        b = { bg = colors.darkgray, fg = colors.gray },
-        c = { bg = colors.darkgray, fg = colors.gray }
+        b = { bg = colors.dark_blue, fg = colors.fg },
+        c = { bg = colors.bg, fg = colors.medium_blue }
     }
 }
